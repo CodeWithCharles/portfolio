b18fda7c261b8aaa481f093338fc7cc1256145b0
Charles POULAIN <charles.probe@gmail.com>
2025-03-14 18:34:04 +0100

    ~ | Normalize CSS & SASS install

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+ 	plugins: [vue()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+					@import "@/scss/_variables.scss";
+					@import "@/scss/_mixins.scss";
+				`
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	}
+});
